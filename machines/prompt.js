c06e15165355b039b7b46e19a61f450c89fd2d89
wi2 <contact@wi2prod.fr>
2015-04-06 22:57:12 +0200

    fix description

--- machines/prompt.js
+++ machines/prompt.js
@@ -2,24 +2,22 @@ module.exports = {
 
   friendlyName: 'Prompt',
   description: 'Simple prompt',
-  extendedDescription: '
-## Install
-```sh
-$ npm install -g machinepack-cli
-```
-(with sudo if error)
-
-## Usage
-```
-$ cli --help
-$ cli --generate file
-```
-Generate a file in bin folder
-```
-$ cli file --hello
-$ cli file --help
-```
-  ',
+  extendedDescription: " ## Install "
++ " ``` "
++ " $ npm install -g machinepack-cli "
++ " ``` "
++ " (with sudo if error)"
++ "  "
++ " ## Usage "
++ " ``` "
++ " $ cli --help"
++ " $ cli --generate file "
++ " ``` "
++ " Generate a file in bin folder "
++ " ``` "
++ " $ cli file --hello "
++ " $ cli file --help "
++ " ``` ",
 
   sync: true,
 
